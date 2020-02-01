29de5177a77e3b718bedb5bdddf50e53a531b28c
Bernard A. Badger <bab9e9@gmail.com>
2020-02-01 11:48:19 -0500

    worries about Image type generality

--- methods/25/exercise-images.go
+++ methods/25/exercise-images.go
@@ -27,4 +27,14 @@ func main() {
 	m := Image{256, 256}
 	pic.ShowImage(&m)
 }
-
+/*
+// This exemplifies the go lang technique where there are no classes, 
+// but you can add "methods" to a type --- functions modifying an implicit argument.
+// This is _only_ an exercise, but it bothers me that the Image type we are creating 
+// doesn't have much "content". I.e., only rows, cols.
+// The functions (methods) defined on the type provide all of the "data",
+//
+// But this means that you cannot set anything about the object except its row,cols 
+// How would you go about transforming an image, for instance?
+//
+*/
