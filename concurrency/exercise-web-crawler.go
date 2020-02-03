73acb5fc723b3858c6f87dded5c3e79fb1be5262
Bernard A. Badger <bab9e9@gmail.com>
2020-02-03 15:48:21 -0500

    Get Safecounter from https://gist.github.com/harryhare/6a4979aa7f8b90db6cbc74400d0beb49

--- concurrency/exercise-web-crawler.go
+++ concurrency/exercise-web-crawler.go
@@ -10,6 +10,23 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+type SafeCounter struct {
+	v   map[string]bool
+	mux sync.Mutex
+}
+var c SafeCounter = SafeCounter{v: make(map[string]bool)}
+func (s SafeCounter)checkvisited(url string)bool{
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	_,ok:=s.v[url]
+	if ok==false {
+		s.v[url]=true
+		return false
+	}
+	return true
+	
+}
+
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
 func Crawl(seen map[string]int, url string, depth int, fetcher Fetcher) {
